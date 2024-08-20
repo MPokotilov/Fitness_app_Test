f92bd15af8237e8be20c0c9baab0114acc6bfb5b
MPokotilov <mpokotilov2@gmail.com>
2024-08-20 15:25:21 +0200

    Added needed Photos for the App

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
+import { ThemeProvider, styled } from "styled-components";
+import { lightTheme } from "./utils/Themes";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={lightTheme}> Hello World</ThemeProvider>
   );
 }
 
