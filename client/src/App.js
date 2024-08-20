5612ab6a26a2bbb388c6afde9583c456c02e11b9
MPokotilov <mpokotilov2@gmail.com>
2024-08-20 16:05:17 +0200

    Added auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import { ThemeProvider, styled } from "styled-components";
 import { lightTheme } from "./utils/Themes";
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Authentication from "./pages/Authentication";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
 
 function App() {
   return (
-    <ThemeProvider theme={lightTheme}> Hello World</ThemeProvider>
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+          <Container>
+            <Authentication />
+          </Container>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
