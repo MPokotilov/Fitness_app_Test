cf31e1df6aeee862be8cb815d7e99e66290af07b
MPokotilov <mpokotilov2@gmail.com>
2024-08-22 12:03:28 +0200

    Revert "Update NavBar.jsx"
    This reverts commit 83ae49dacd1c92caef8295e62b3d82ee1fba5df1.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -74,7 +74,6 @@ const Navlink = styled(NavLink)`
   cursor: pointer;
   transition: all 1s slide-in;
   text-decoration: none;
-  white-space: nowrap;
   &:hover {
     color: ${({ theme }) => theme.primary};
   }
